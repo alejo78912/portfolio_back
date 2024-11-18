2ca73b6fcd1d4a4a63fcdecf08a341150e833740
Alejandro Valencia Valencia <alejandrovalenciavalencia@Laptop-de-Alejandro.local>
2024-11-17 19:06:15 -0500

    cors and token error

--- portfolio_back/src/main/java/com/portfolio/alejandro/Config/SecurityConfig.java
+++ portfolio_back/src/main/java/com/portfolio/alejandro/Config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));  // Permitir solicitudes desde Angular
+        configuration.setAllowedOrigins(List.of("*"));  // Permitir solicitudes desde Angular
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
--- portfolio_back/src/main/java/com/portfolio/alejandro/Config/Webconfig.java
+++ portfolio_back/src/main/java/com/portfolio/alejandro/Config/Webconfig.java
@@ -10,7 +10,7 @@ public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("https://portfolio.alejo78912.com")  
+                .allowedOrigins("*")  
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  
                 .allowedHeaders("*")  
                 .allowCredentials(true);  
